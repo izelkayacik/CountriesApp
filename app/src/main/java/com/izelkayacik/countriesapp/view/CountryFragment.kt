4e491737794033b9ad13aea192f146001f563eef
izelkayacik <izelkayacik@hotmail.com>
2021-12-03 20:04:31 +0300

    MVVM

--- app/src/main/java/com/izelkayacik/countriesapp/view/CountryFragment.kt
+++ app/src/main/java/com/izelkayacik/countriesapp/view/CountryFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.izelkayacik.countriesapp.R
+import com.izelkayacik.countriesapp.viewmodel.CountryViewModel
+import kotlinx.android.synthetic.main.fragment_country.*
 
 
 class CountryFragment : Fragment() {
 
+    private lateinit var viewModel : CountryViewModel
     private var countryUuid = 0
 
 
@@ -29,9 +36,26 @@ class CountryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProviders.of(this).get(CountryViewModel::class.java)
+        viewModel.getDataFromRoom()
+
         arguments?.let {
             countryUuid = CountryFragmentArgs.fromBundle(it).countryUuid
         }
+        observeLiveData()
+    }
+
+    private fun observeLiveData(){
+        viewModel.countryLiveData.observe(viewLifecycleOwner, Observer { country->
+            country?.let {
+                countryName.text = country.countryName
+                countryCapital.text = country.countryCapital
+                countryCurrency.text = country.countryCurrency
+                countryLanguage.text = country.countryLanguage
+                countryRegion.text = country.countryRegion
+
+            }
+        })
 
     }
 
---
+++ app/src/main/java/com/izelkayacik/countriesapp/viewmodel/FeedViewModel.kt
@@ -0,0 +1,25 @@
+package com.izelkayacik.countriesapp.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.izelkayacik.countriesapp.model.Country
+
+class FeedViewModel : ViewModel(){
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryError = MutableLiveData<Boolean>()
+    val countryLoading = MutableLiveData<Boolean>()
+
+    fun refreshData(){
+
+        val country = Country("Turkey", "Asia", "Ankara", "TRY", "Turkish", "www.ss.com")
+        val country2 = Country("France", "Europe", "Paris", "EUR", "French", "www.ss.com")
+        val country3 = Country("German", "Europe", "Berlin", "EUR", "German", "www.ss.com")
+
+        val countryList = arrayListOf<Country>(country, country2, country3)
+
+        countries.value = countryList
+        countryError.value = false
+        countryLoading.value = false
+    }
+}
