f8a397e473333be54c8de457f5fb6848483e10b5
izelkayacik <izelkayacik@hotmail.com>
2021-12-07 17:45:10 +0300

    @ColumnInfo

--- app/src/main/java/com/izelkayacik/countriesapp/model/Model.kt
+++ app/src/main/java/com/izelkayacik/countriesapp/model/Model.kt
@@ -1,19 +1,38 @@
 package com.izelkayacik.countriesapp.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class Country(
 
     //value nin içerisindeki isim JSON da ki ile aynı olmak zorunda
+    @ColumnInfo(name = "name")
     @SerializedName("name")
     val countryName: String?,
+
+    @ColumnInfo()
     @SerializedName("region")
     val countryRegion: String?,
+
+    @ColumnInfo(name = "capital")
     @SerializedName("capital")
     val countryCapital: String?,
+
+    @ColumnInfo(name = "currency")
     @SerializedName("currency")
     val countryCurrency: String?,
+
+    @ColumnInfo(name = "language")
     @SerializedName("language")
     val countryLanguage: String?,
+
+    @ColumnInfo(name = "flag")
     @SerializedName("flag")
-    val imageUrl: String?)
+    val imageUrl: String?) {
+
+    @PrimaryKey(autoGenerate = true)
+    var uuid: Int = 0
+}
